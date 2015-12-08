73a28e5bd9eb13a88b18d0f840d9ac0b88122ae8
Judy Nguyen <jtnguyen.jn@gmail.com>
2015-12-07 21:31:06 -0500

    struct as JSON

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 extern crate iron;
+extern crate serde;
 
 use iron::prelude::*;
 use iron::status;
+use serde::json;
+
+struct Greeting {
+  msg:string
+ }
 
 fn main() {
   fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello World!")))
+    let greeting = Greeting { msg: "Hello, World".to_string() };
+    let payload = json::to_string(&greeting).unwrap();
+    Ok(Response::with((status::Ok, payload))
   }
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-      println!("On 3000");
+  Iron::new(hello_world).http("localhost:3000").unwrap();
+    println!("On 3000");
  }
